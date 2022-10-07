b9e5c5dfb40e5c6dd6f5e97f968118e5a616ffcc
wilk.gillian <wilk.silva@mt.senac.br>
2022-10-07 10:22:25 -0300

    ts

--- server.py
+++ server.py
@@ -1,14 +1,8 @@
-import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI(debug=True)
+app = FastAPI()
 
 @app.get("/")
 
 async def index():
-    return {"Hello World!"}
-
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
+    return {"Message": "Success"}
