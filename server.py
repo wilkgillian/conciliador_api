0a026a7bde0cbc47debd9d018dd3de3d3eb10ccf
wilk.gillian <wilk.silva@mt.senac.br>
2022-10-13 12:33:26 -0300

    aws

--- server.py
+++ server.py
@@ -3,13 +3,24 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 import databases
+import psycopg2
 import sqlalchemy
+from sqlalchemy_utils import URLType
 from pydantic import BaseModel
 import os
+import boto3
 from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = (os.environ["DATABASE"])
+bucket_name = (os.environ["AWS_BUCKET_NAME"])
+
+s3 = boto3.client(
+    service_name="s3",
+    aws_access_key_id=(os.environ["AWS_ACCESS_KEY_ID"]),
+    aws_secret_access_key=(os.environ["AWS_SECRET_ACCESS_KEY"]),
+    region_name='sa-east-1'
+)
 
 database = databases.Database(DATABASE_URL)
 
@@ -20,7 +31,7 @@ files = sqlalchemy.Table(
     metadata,
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("name", sqlalchemy.String, nullable=False,),
-    sqlalchemy.Column("size", sqlalchemy.Integer, nullable=False,),
+    sqlalchemy.Column("file_url", URLType),
     sqlalchemy.Column("upload_at", sqlalchemy.String,
                       nullable=False, default=datetime.now())
 )
@@ -33,7 +44,7 @@ metadata.create_all(engine)
 class Files(BaseModel):
     id: int
     name: str
-    upload_at: str 
+    upload_at: str
 
 
 class FilesIn(BaseModel):
@@ -64,25 +75,38 @@ async def shutdown():
 
 @app.get("/files", response_model=List)
 async def read_files():
-    query = files.select()
+    query = files.select().order_by("id")
     return await database.fetch_all(query)
 
 
 @app.post("/file/upload")
-async def upload_file(arquivo: UploadFile = File(...)):
+async def upload_file(arquivo: UploadFile):
     upload_at = datetime.now()
-    query = files.insert().values(name=arquivo.filename, upload_at=str(upload_at))
+
+    # bucket = s3. Bucket(bucket_name)
+    s3.upload_fileobj(arquivo.file, bucket_name, arquivo.filename)
+
+    uploaded_file_url = f"https://{bucket_name}.s3.amazonaws.com/{str(arquivo.filename)}"
+
+    query = files.insert().values(name=str(arquivo.filename),
+                                  file_url=uploaded_file_url, upload_at=str(upload_at))
+
     last_record_id = await database.execute(query)
-    return {"file": arquivo.filename, "id": last_record_id, "upload_at": upload_at}
+
+    return {"name": arquivo.filename, "url_file": uploaded_file_url, "id": last_record_id, "upload_at": upload_at}
 
 
 @app.put("/file/{file_id}")
-async def update_file(file_id: int, arquivo: UploadFile = File(...)):
+async def update_file(file_id: int, arquivo: UploadFile):
     upload_at = datetime.now()
+
+    s3.upload_fileobj(arquivo.file, bucket_name, arquivo.filename)
+
+    uploaded_file_url = f"https://{bucket_name}.s3.amazonaws.com/{str(arquivo.filename)}"
     query = files.update().where(files.columns.id == file_id).values(
-        name=arquivo.filename, upload_at=str(upload_at))
+        name=arquivo.filename, file_url=uploaded_file_url, upload_at=str(upload_at))
     await database.execute(query)
-    return {"file": arquivo.filename, "id": file_id, "upload_at": str(upload_at)}
+    return {"id": file_id, "name": arquivo.filename, "file_url": uploaded_file_url, "upload_at": str(upload_at)}
 
 
 @app.delete("/file/delete/{file_id}")
@@ -90,3 +114,14 @@ async def update_file(file_id: int):
     query = files.delete().where(files.columns.id == file_id)
     await database.execute(query)
     return {"message": "the file with id={} deleted successfully".format(file_id)}
+
+
+@app.get("/file/conciliado")
+async def conciliado():
+    query = "SELECT * FROM files ORDER BY upload_at DESC LIMIT 5"
+    await database.execute(query)
+    # list_users = cur.fetchall()
+    # query = files.select().order_by("upload_at").limit(3)
+    # query = files.select().order_by("upload_at")
+    # await database.execute(query)
+    return await database.fetch_all(query)
