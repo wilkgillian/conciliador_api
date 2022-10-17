add680d49ca22baca1469665f2f6f8e7867e49b4
wilk.gillian <wilk.silva@mt.senac.br>
2022-10-17 12:40:30 -0300

    fin...

--- server.py
+++ server.py
@@ -1,13 +1,11 @@
 from datetime import datetime
-import numpy as np
+import json
 import openpyxl
-import requests
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 import databases
 import pandas as pd
-import psycopg2
 import sqlalchemy
 from sqlalchemy_utils import URLType
 from pydantic import BaseModel
@@ -50,11 +48,6 @@ class Files(BaseModel):
     name: str
     file_url: str
     upload_at: str
-
-
-# class FilesIn(BaseModel):
-#     name: str
-#     upload_at: str
 
 
 app = FastAPI()
@@ -127,72 +120,97 @@ async def conciliado():
     await database.execute(query)
     var = await database.fetch_all(query)
     vendas_cielo_url = str(tuple(var[4].values())).replace("('", "")
-    vendas_cielo_url_replace = vendas_cielo_url.replace("',)","")
+    vendas_cielo_url_replace = vendas_cielo_url.replace("',)", "")
     vendas_sig_url = str(tuple(var[3].values())).replace("('", "")
-    vendas_sig_url_replace = vendas_sig_url.replace("',)","")
+    vendas_sig_url_replace = vendas_sig_url.replace("',)", "")
     recebimentos_cielo_url = str(tuple(var[2].values())).replace("('", "")
-    recebimentos_cielo_url_replace = recebimentos_cielo_url.replace("',)","")
+    recebimentos_cielo_url_replace = recebimentos_cielo_url.replace("',)", "")
     recebimentos_sig_url = str(tuple(var[1].values())).replace("('", "")
-    recebimentos_sig_url_replace = recebimentos_sig_url.replace("',)","")
+    recebimentos_sig_url_replace = recebimentos_sig_url.replace("',)", "")
     mxm_url = str(tuple(var[0].values())).replace("('", "")
-    mxm_url_replace = mxm_url.replace("',)","")
-   
+    mxm_url_replace = mxm_url.replace("',)", "")
+
     df_vendas_cielo = pd.read_excel(vendas_cielo_url_replace, usecols=[
-                             "Código de autorização", "Valor da venda"])
+        "Código de autorização", "Valor da venda"])
     df_vendas_sig = pd.read_excel(vendas_sig_url_replace, usecols=[
-                           "Aut. de Venda", "Valor Proporcional"])
+        "Aut. de Venda", "Valor Proporcional"])
     df_recebimentos_cielo = pd.read_excel(recebimentos_cielo_url_replace, usecols=[
-                                   "Data de pagamento", "Código de autorização", "Valor bruto"])
+        "Data de pagamento", "Código de autorização", "Valor bruto"])
     df_recebimentos_sig = pd.read_excel(recebimentos_sig_url_replace, usecols=[
-                                 "Aut. de Venda", "Valor Proporcional"])
-    df_mxm = pd.read_excel(mxm_url_replace, usecols=['Data', 'Histórico', 'Débito', 'Crédito'], skipfooter=1)
-    
+        "Aut. de Venda", "Valor Proporcional"])
+    df_mxm = pd.read_excel(mxm_url_replace, usecols=[
+                           'Data', 'Histórico', 'Débito', 'Crédito'], skipfooter=1)
+
     grup_vendas_cielo = df_vendas_cielo.groupby(
-    pd.Grouper(key='Código de autorização')).sum()
+        pd.Grouper(key='Código de autorização')).sum()
     grup_vendas_sig = df_vendas_sig.groupby(
-    pd.Grouper(key='Aut. de Venda')).sum()
+        pd.Grouper(key='Aut. de Venda')).sum()
     grup_recebimentos_cielo = df_recebimentos_cielo.groupby(
-    pd.Grouper(key='Código de autorização')).sum()
+        pd.Grouper(key='Código de autorização')).sum()
     grup_recebimentos_sig = df_recebimentos_sig.groupby(
-    pd.Grouper(key='Aut. de Venda')).sum()
+        pd.Grouper(key='Aut. de Venda')).sum()
     grup_recebimentos_cielo2 = df_recebimentos_cielo.groupby(
-    pd.Grouper(key='Data de pagamento')).sum()
+        pd.Grouper(key='Data de pagamento')).sum()
     grup_mxm = df_mxm.groupby(pd.Grouper(key="Data")).sum()
-    
+
     vendas_cieloXsig = pd.merge(pd.DataFrame(
-    grup_vendas_cielo), pd.DataFrame(grup_vendas_sig), left_on="Código de autorização", right_on="Aut. de Venda", right_index=True)
+        grup_vendas_cielo), pd.DataFrame(grup_vendas_sig), left_on="Código de autorização", right_on="Aut. de Venda", right_index=True)
 
     recebimentos_cieloXsig = pd.merge(pd.DataFrame(grup_recebimentos_cielo), pd.DataFrame(
-    grup_recebimentos_sig), left_on="Código de autorização", right_on="Aut. de Venda", right_index=True)
+        grup_recebimentos_sig), left_on="Código de autorização", right_on="Aut. de Venda", right_index=True)
 
     razao_contabilXcielo = pd.merge(pd.DataFrame(grup_recebimentos_cielo2), pd.DataFrame(
-    grup_mxm), left_on="Data de pagamento", right_on="Data", right_index=True)
-    
+        grup_mxm), left_on="Data de pagamento", right_on="Data", right_index=True)
+
     wb = openpyxl.Workbook()
     wb.create_sheet('diferencas_vendas_cieloxsig')
     diferencas_vendas_cieloxsig = wb['diferencas_vendas_cieloxsig']
-    diferencas_vendas_cieloxsig.append(['aut_pagamento', 'valor_cielo', 'valor_sig', 'diferenca'])
-    
+    diferencas_vendas_cieloxsig.append(
+        ['id', 'aut_pagamento', 'valor_cielo', 'valor_sig', 'diferenca'])
+
     for index, row in vendas_cieloXsig.iterrows():
-      v_cielo = row["Valor da venda"]
-      v_sig = row["Valor Proporcional"]
-      if round(v_cielo) != round(v_sig):
-        diferenca = round(v_cielo, 2) - round(v_sig, 2)
-        diferencas_vendas_cieloxsig.append([str(
-            index), v_cielo, v_sig, str(round(diferenca, 2)).replace("-", "")])
-        # print(index, round(v_cielo, 2), round(v_sig, 2),
-        #       "Diferença de: ", round(diferenca, 2))
+        v_cielo = row["Valor da venda"]
+        v_sig = row["Valor Proporcional"]
+        if round(v_cielo) != round(v_sig):
+            diferenca = round(v_cielo, 2) - round(v_sig, 2)
+            diferencas_vendas_cieloxsig.append([str(
+                index), str(
+                index), v_cielo, v_sig, str(round(diferenca, 2)).replace("-", "")])
+
+    wb.create_sheet('diferencas_recebimentos_cieloxsig')
+    diferencas_recebimentos_cieloxsig = wb['diferencas_recebimentos_cieloxsig']
+    diferencas_recebimentos_cieloxsig.append(
+        ['id', 'aut_pagamento', 'valor_cielo', 'valor_sig', 'diferenca'])
+
+    for index, row in recebimentos_cieloXsig.iterrows():
+        r_cielo = row["Valor bruto"]
+        r_sig = row["Valor Proporcional"]
+        if round(r_cielo) != round(r_sig):
+            diferenca = round(r_cielo, 2) - round(r_sig, 2)
+            diferencas_recebimentos_cieloxsig.append([str(
+                index), str(
+                index), r_cielo, r_sig, str(round(diferenca, 2)).replace("-", "")])
+
+    wb.create_sheet('diferencas_mxm_cielo')
+    diferenca_cieloxmxm = wb['diferencas_mxm_cielo']
+    diferenca_cieloxmxm.append(
+        ['id', 'data_recebimento', 'valor_cielo', 'valor_mxm', 'diferenca'])
+
+    for index, row in razao_contabilXcielo.iterrows():
+        v_mxm = row["Crédito"]
+        v_cielo_m = row["Valor bruto"]
+        if round(v_cielo_m) != round(v_mxm):
+            diferenca = round(v_cielo_m, 2) - round(v_mxm, 2)
+            diferenca_cieloxmxm.append([str(index), str(index), v_cielo_m, v_mxm, str(round(diferenca, 2)).replace("-", "")])
+
     wb.save(filename="teste.xlsx")
-    excel = pd.read_excel("teste.xlsx", "diferencas_vendas_cieloxsig")
-    df = pd.DataFrame(excel).to_json(orient="records")
-    print(df)
+    dif_vendas = pd.read_excel("teste.xlsx", "diferencas_vendas_cieloxsig", dtype="string")
+    dif_recebimentos = pd.read_excel("teste.xlsx", "diferencas_recebimentos_cieloxsig", dtype="string")
+    dif_razao_contabil = pd.read_excel("teste.xlsx", "diferencas_mxm_cielo", dtype="string")
+  
+    d_v = {"dif_vendas_cielo_sig": [dif_vendas], "dif_recebimentos_cielo_sig": [dif_recebimentos], "dif_razao_contabil": [dif_razao_contabil]}
+    json_dataframe = pd.DataFrame(d_v).to_json(orient="records")
     
-    # print(grup_recebimentos_cielo2)
-    # print(grup_mxm)
-    # print("*************")
-    # print(vendas_cieloXsig)
+    json_obj = str(json_dataframe).replace("\\","")
     
-    # print(recebimentos_cieloXsig)
-    # print(razao_contabilXcielo)
-    
-    return df
+    return json.loads(json_obj)
